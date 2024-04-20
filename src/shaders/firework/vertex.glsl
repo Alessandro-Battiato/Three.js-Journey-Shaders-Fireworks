f854e1c9ddf61057a8e5ff22b04b7dd4034f3832
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-04-20 14:56:27 +0200

    feat: dynamic size implemented

--- src/shaders/firework/vertex.glsl
+++ src/shaders/firework/vertex.glsl
@@ -24,12 +24,18 @@ void main() {
     fallingProgress = 1.0 - pow(1.0 - fallingProgress, 3.0);
     newPosition.y -= fallingProgress * 0.2;
 
+    // Scaling
+    float sizeOpeningProgress = remap(uProgress, 0.0, 0.125, 0.0, 1.0);
+    float sizeClosingProgress = remap(uProgress, 0.125, 1.0, 1.0, 0.0);
+    float sizeProgress = min(sizeOpeningProgress, sizeClosingProgress);
+    sizeProgress = clamp(sizeProgress, 0.0, 1.0);
+
     // Final position
     vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     gl_Position = projectionMatrix * viewPosition;
 
     // Final particle size
-    gl_PointSize = uSize * uResolution.y * aSize; // fix particles size according to vertical FOV so it scales if window is resized vertically (not horizontally)
+    gl_PointSize = uSize * uResolution.y * aSize * sizeProgress; // fix particles size according to vertical FOV so it scales if window is resized vertically (not horizontally)
     gl_PointSize *= 1.0 / - viewPosition.z; // perspective based size
 }
