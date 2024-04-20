fc6a0ccb86435e2bc6cf25dc81dc9c60d1869d88
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-04-20 14:45:49 +0200

    feat: implemented position remap value as to improve the animation and make the particles go outwards

--- src/shaders/firework/vertex.glsl
+++ src/shaders/firework/vertex.glsl
@@ -1,11 +1,23 @@
 uniform float uSize;
 uniform vec2 uResolution;
+uniform float uProgress;
 
 attribute float aSize;
 
+float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax)
+{
+    return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
+}
+
 void main() {
+    vec3 newPosition = position; // you can't edit the real position so you create a new one
+
+    // Exploding
+    float explodingProgress = remap(uProgress, 0.0, 0.1, 0.0, 1.0);
+    newPosition *= explodingProgress;
+
     // Final position
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     gl_Position = projectionMatrix * viewPosition;
 
